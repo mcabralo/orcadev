bfe3b24dd0aa9cc3bed32329eb766006bc8bff03
Matheus Cabral <44450101+mcabralo@users.noreply.github.com>
2021-01-04 22:51:21 -0300

    Initial Commit + Auth + AdmiLte

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home')->middleware('auth');
