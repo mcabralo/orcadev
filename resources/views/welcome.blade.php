66423928a073ad870b102542f628af98511a7378
Matheus Cabral <44450101+mcabralo@users.noreply.github.com>
2021-01-05 13:05:19 -0300

    Ajuste de Views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,39 +74,16 @@
         @if (Route::has('login'))
             <div class="top-right links">
                 @auth
-                    <a href="{{ url('/home') }}">Home</a>
+                    <a href="{{ url('/home') }}">Página Inicial</a>
                 @else
-                    <a href="{{ route('login') }}">Login</a>
+                    <a href="{{ route('login') }}">Logar</a>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('register') }}">Registrar</a>
                     @endif
                 @endauth
             </div>
         @endif
-
-        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="../../images/img1.jpg" class="d-block w-100" alt="Pruu">
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </a>
-        </div>
-
 
     </div>
 </body>
