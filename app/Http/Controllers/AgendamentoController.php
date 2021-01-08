a12d9508afefb63542a2e831caf60b4135959967
Matheus Cabral <44450101+mcabralo@users.noreply.github.com>
2021-01-07 22:28:48 -0300

    Médicos

--- app/Http/Controllers/AgendamentoController.php
+++ app/Http/Controllers/AgendamentoController.php
@@ -3,15 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class AgendamentoController extends Controller
 {
-    public function index(Request $request) {
+    public function index() {
+        $idUsuario = Auth::user()->id;
 
-        $request = DB::table('agendamento');
-        // dd($request);
+        // $agendamentos = DB::table('agendamentos')
+        // ->where('medId' , $idUsuario)
+        // ->get();
 
-        return view('agendamento.index', compact('request'));
+        $agendamentos = DB::table('agendamentos AS a')
+        ->join('users AS u', 'a.pacId', '=', 'u.id')
+        ->where('a.medId' , $idUsuario)
+        ->get();
+
+        // dd($agendamentos);
+
+
+
+        // dd($request, $idUsuario);
+
+        return view('agendamento.index', compact('agendamentos'));
     }
 }
--- resources/views/agendamento/index.blade.php
+++ resources/views/agendamento/index.blade.php
@@ -9,15 +9,69 @@
 @section('content')
     <div class="card card-info">
         <div class="card-header">
-            <h3 class="card-title">Consultas Agendadas</h3>
+            @foreach ($agendamentos as $agendamento)
+                @if ($loop->last)
+                    <h3 class="card-title">Você tem {{ $loop->count }} consultas agendadas</h3>
+                @endif
+            @endforeach
         </div>
         <!-- /.card-header -->
         <div class="card-body">
-            The body of the card
+            <ul class="list-group">
+                {{-- @foreach ($agendamentos as $agendamento)
+                    --}}
+                    {{-- <div class="container">
+                        <div class="row">
+                            <div class="col-6 col-sm-3">
+                                {{ $agendamento->data }}
+                            </div>
+                            <div class="col-3 col-sm-3">
+                                {{ $agendamento->horaInicio }}
+                            </div>
+                            <div class="col-3 col-sm-3">
+                                {{ $agendamento->horaTermino }}
+                            </div>
+                            <div class="col-12 col-sm-3">
+                                {{ $agendamento->medId }}
+                            </div>
+                        </div>
+                    </div> --}}
+                    <div class="conteiner">
+                        <div class="row">
+                            <table class="table table-hover table-sm">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">N</th>
+                                        <th scope="col">Data da Consulta</th>
+                                        <th scope="col">Hora de Início</th>
+                                        <th scope="col">Hora Esperada de Término</th>
+                                        <th scope="col">Paciente</th>
+                                    </tr>
+                                </thead>
+
+                                @foreach ($agendamentos as $agendamento)
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row"> {{ $loop->iteration }} </th>
+                                            <td scope="col">{{ $agendamento->data }}</td>
+                                            <td scope="col">{{ $agendamento->horaInicio }}</td>
+                                            <td scope="col">{{ $agendamento->horaTermino }}</td>
+                                            <td scope="col">{{ $agendamento->name }}</td>
+                                        </tr>
+                                    </tbody>
+
+                                @endforeach
+                            </table>
+                        </div>
+                    </div>
+
+
+            </ul>
         </div>
+
         <!-- /.card-body -->
         <div class="card-footer">
-            The footer of the card
+            *É recomendado a chegada com pelo menos 15min de antecedência. Horário sujeito a lotação.
         </div>
         <!-- /.card-footer -->
     </div>
